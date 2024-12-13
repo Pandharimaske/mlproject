012894772031221344fc331f6a360c211433ab14
Pandhari <pandhari2527@gmail.com>
2024-12-14 03:30:16 +0530

    Model Trainer Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation , DataTransformationConfig
+from src.components.model_trainer import ModelTrainer , ModelTrainerConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifact' , 'train.csv')
@@ -49,7 +50,10 @@ if __name__ == "__main__":
     train_data_path , test_data_path = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path=train_data_path , test_path=test_data_path)
+    train_arr , test_arr , preprocessor_file_path = data_transformation.initiate_data_transformation(train_path=train_data_path , test_path=test_data_path)
+
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr=train_arr , test_arr=test_arr , preprocessor_path=preprocessor_file_path)
 
     
 
